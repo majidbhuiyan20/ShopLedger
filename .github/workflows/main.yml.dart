ca4cfdffa690153ef6155339630c3b4af193a5fa
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2026-05-13 10:25:30 +0600

    update github release workflow token reference

--- .github/workflows/main.yml.dart
+++ .github/workflows/main.yml.dart
@@ -41,4 +41,4 @@ jobs:
         with:
           artifacts: "build/app/outputs/flutter-apk/*.apk,build/app/outputs/bundle/release/*.aab,build/app/outputs/apk/profile/*.apk,build/ios/iphoneos/app.ipa"
           tag: v1.0.${{ github.run_number }}-${{ github.event.inputs.branch_name || 'main' }}
-          token: ${{ secrets.GITHUB_TOKEN }}
+          token: ${{ secrets.TOKEN }}
