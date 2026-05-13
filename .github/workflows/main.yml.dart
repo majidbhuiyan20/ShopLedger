3f9efdd18c58fcac56707f756cf1ee7b7ab71bf4
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2026-05-13 10:22:23 +0600

    update workflow to split apk and add release step

--- .github/workflows/main.yml.dart
+++ .github/workflows/main.yml.dart
@@ -28,10 +28,17 @@ jobs:
 
       - run: flutter pub get
 
-      - run: flutter build apk --release
+      - run: flutter build apk --release --split-per-abi
 
       - name: Upload Artifact
         uses: actions/upload-artifact@v4
         with:
           name: app-release
           path: build/app/outputs/flutter-apk/app-release.apk
+
+      - name: Push to Releases
+        uses: ncipollo/release-action@v1
+        with:
+          artifacts: "build/app/outputs/flutter-apk/*.apk,build/app/outputs/bundle/release/*.aab,build/app/outputs/apk/profile/*.apk,build/ios/iphoneos/app.ipa"
+          tag: v1.0.${{ github.run_number }}-${{ github.event.inputs.branch_name || 'main' }}
+          token: ${{ secrets.GITHUB_TOKEN }}
